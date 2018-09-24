97feb5b8d063177820387ce71552b207b4e4f837
iamsachinvanjari <iamsachin.vanjari@gmail.com>
2018-09-24 14:47:54 +0530

    third commit

---
+++ src/main/java/first/Google2.java
@@ -0,0 +1,29 @@
+package first;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Google2 {
+	
+
+	static WebDriver driver;
+	@BeforeTest
+	public void m1() {
+		System.setProperty("geckodriver", "/home/sachin/Downloads/Selenium/geckodriver");
+		driver = new FirefoxDriver();
+		driver.get("https://www.google.com/");
+		System.out.println(driver.getTitle());
+	}
+	
+	@Test
+	public void m2() {
+	
+		driver.findElement(By.name("q")).sendKeys("selenium hq");
+		driver.findElement(By.name("btnK")).click();
+		System.out.println(driver.getTitle());
+		
+	}
+}
